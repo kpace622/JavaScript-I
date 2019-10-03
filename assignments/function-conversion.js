3eafef72504a87a8d093ff023cc276474d3d509e
Kyle Pace <kpace2296@gmail.com>
2019-10-02 22:29:00 -0500

    finished project

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -1,20 +1,32 @@
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
 
+
+let myFunction = () => console.log("function was invoked!");
+myFunction();
 // let myFunction = function () {
 // console.log("Function was invoked!");
 // };
 // myFunction();
 
+
+let anotherFunction = (param) => { return param; } 
+console.log(anotherFunction("example"));
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
 
+
+let add = (param1, param2) => { return param1 + param2; }
+console.log(add(1,2));
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+
+let subtract = (param1, param2) => { return param1 - param2; }
+console.log(subtract(1,2));
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
